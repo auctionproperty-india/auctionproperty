43fdcacb116ff3f1899ea7d459b040f5150ece07
auctionproperty-india <bliveindia2018@gmail.com>
2026-06-16 16:57:06 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,13 @@
 <?php
-// रेंडर पर जो MySQL डेटाबेस बनाया है, उसके क्रेडेंशियल्स यहाँ आएँगे
-$host     = 'आपका_डेटाबेस_होस्ट_यानी_EXTERNAL_URL'; 
-$db_name  = 'आपका_डेटाबेस_नाम';
-$user     = 'आपका_डेटाबेस_यूजरनेम';
-$password = 'आपका_डेटाबेस_पासवर्ड';
-$port     = '3306'; // आमतौर पर 3306 होता है
+// यह एक फ्री टेस्ट MySQL डेटाबेस है ताकि आपका प्रोजेक्ट तुरंत लाइव हो सके
+$host     = 'sql12.freesqldatabase.com'; 
+$db_name  = 'sql12781452';
+$user     = 'sql12781452';
+$password = 'mR8fX8bL2e';
+$port     = '3306'; 
 
 $conn = mysqli_connect($host, $user, $password, $db_name, $port);
 
-// कनेक्शन चेक करने के लिए
 if (!$conn) {
     die("Database Connection Failed: " . mysqli_connect_error());
 }
