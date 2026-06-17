d4574163c0411a65ea67f31ab6801f08ed101668
auctionproperty-india <bliveindia2018@gmail.com>
2026-06-17 20:48:59 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,11 @@
 <?php
-print_r(PDO::getAvailableDrivers());
+$db_url = "postgres://auctionproperty_p917_user:JYJZAvIWxQymTwDzCN4lWZo3LdAOqNWM@dpg-cuv66rtu4l7c739m13sg-a.singapore-postgres.render.com:5432/auctionproperty_p917";
+
+try {
+    $conn = new PDO($db_url);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Connection Failed: " . $e->getMessage());
+}
 ?>
