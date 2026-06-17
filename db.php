183c0c93f585ce72807787ce44236bae772fe6be
auctionproperty-india <bliveindia2018@gmail.com>
2026-06-17 21:29:43 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,16 @@
 <?php
-$db_url = "postgres://auctionproperty_p917_user:JYJZAvIWxQymTwDzCN4lWZo3LdAOqNWM@dpg-cuv66rtu4l7c739m13sg-a.singapore-postgres.render.com:5432/auctionproperty_p917";
+$host = "dpg-cuv66rtu4l7c739m13sg-a.singapore-postgres.render.com";
+$dbname = "auctionproperty_p917";
+$user = "auctionproperty_p917_user";
+$password = "JYJZAvIWxQymTwDzCN4lWZo3LdAOqNWM";
+$port = "5432";
 
 try {
-    $conn = new PDO($db_url);
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $conn = new PDO($dsn, $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    // Connection successful
 } catch (PDOException $e) {
-    die("Connection Failed: " . $e->getMessage());
+    die("Database Connection Error: " . $e->getMessage());
 }
 ?>
