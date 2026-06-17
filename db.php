ca0b38b1fc85d772f4f2d1bd0fbe2b24270b4278
auctionproperty-india <bliveindia2018@gmail.com>
2026-06-17 10:20:42 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,12 @@
 <?php
-// रेंडर PostgreSQL कनेक्शन PDO के जरिए
-$dbopts = parse_url("postgresql://admin:JYJZAvIWxQymTwDzCN4lWZo3LdAOqNWM@dpg-d8ok6lflk1mc739ce1j0-a.oregon-postgres.render.com/auction_db_r1hx");
+// रेंडर PostgreSQL कनेक्शन स्ट्रिंग
+$database_url = "postgresql://admin:JYJZAvIWxQymTwDzCN4lWZo3LdAOqNWM@dpg-d8ok6lflk1mc739ce1j0-a.oregon-postgres.render.com/auction_db_r1hx";
+
+$dbopts = parse_url($database_url);
 
 $host = $dbopts["host"];
-$port = $dbopts["port"];
+// अगर यूआरएल में पोर्ट नहीं है, तो अपने आप डिफ़ॉल्ट 5432 पोर्ट ले लेगा, एरर नहीं आएगी
+$port = isset($dbopts["port"]) ? $dbopts["port"] : "5432"; 
 $user = $dbopts["user"];
 $pass = $dbopts["pass"];
 $dbname = ltrim($dbopts["path"], '/');
@@ -11,7 +14,7 @@ $dbname = ltrim($dbopts["path"], '/');
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn = $pdo; 
+    $conn = $pdo; // आपके पुराने कोड के सपोर्ट के लिए
 } catch (PDOException $e) {
     die("Database Connection Failed: " . $e->getMessage());
 }
