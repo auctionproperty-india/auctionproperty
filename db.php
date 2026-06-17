26528f95a0716f7ec1becd1d1b7ccf7bb64dadca
auctionproperty-india <bliveindia2018@gmail.com>
2026-06-17 19:58:25 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,15 @@
+<?php
+// Database Connection Settings
+$host     = "dpg-cuv66rtu4l7c739m13sg-a.singapore-postgres.render.com";
+$dbname   = "auctionproperty_p917";
+$user     = "auctionproperty_p917_user";
+$password = "JYJZAvIWxQymTwDzCN4lWZo3LdAOqNWM";
+$port     = "5432";
 
+try {
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database Connection Error: " . $e->getMessage());
+}
+?>
