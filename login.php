1d5231a34aa7ba529ea77e7f1fa7edfb916c63bd
auctionproperty-india <bliveindia2018@gmail.com>
2026-06-18 16:30:30 +0530

    Update login.php

--- login.php
+++ login.php
@@ -8,8 +8,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if($user && password_verify($password, $user['password'])){
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['user_name'] = $user['name'];
-        header("Location: index.php");
-        exit;
+        header("Location: dashboard.php");        exit;
     } else {
         $error = "❌ गलत Email या Password!";
     }
