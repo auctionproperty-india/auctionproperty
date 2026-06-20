b4974b7238ec4adb99b6b46d7f36d031f26a7b52
auctionproperty-india <bliveindia2018@gmail.com>
2026-06-20 20:03:21 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,8 @@
 <?php
+if(session_status() == PHP_SESSION_NONE) session_start();
+if(!isset($_SESSION['user_id'])) { header("Location: login.php"); exit; }
 require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/functions.php';
-
-if(!isset($_SESSION['user_id'])) { 
-    header("Location: login.php"); 
-    exit; 
-}
 
 $role = $_SESSION['role'] ?? 'user';
 
