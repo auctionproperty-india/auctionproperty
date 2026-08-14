33ba4c5d8853863ff0579c1d90d439e5558da7c6
auctionproperty-india <bliveindia2018@gmail.com>
2026-08-14 11:25:53 +0530

    Update user_packages.php

--- user_packages.php
+++ user_packages.php
@@ -91,7 +91,8 @@ $has_pending = $pending_check->rowCount() > 0;
                         <?php elseif ($has_pending): ?>
                             <div class="badge bg-warning text-dark w-100 mt-2">⏳ Pending Request</div>
                         <?php else: ?>
-                            <a href="buy_subscription.php?package_id=<?= $pkg['id'] ?>" class="btn btn-primary w-100 btn-sm mt-2">Buy Now</a>
+                            <!-- 🔁 CHANGE: "Buy Now" अब payment_info.php पर जाएगा -->
+                            <a href="payment_info.php?package_id=<?= $pkg['id'] ?>" class="btn btn-primary w-100 btn-sm mt-2">Buy Now</a>
                         <?php endif; ?>
                     </div>
                 </div>
