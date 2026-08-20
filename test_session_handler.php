54da24eaa3bd3d9452df1f46bfc593cde4ec0c7e
auctionproperty-india <bliveindia2018@gmail.com>
2026-08-20 18:03:16 +0530

    Delete test_session_handler.php

--- test_session_handler.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'db.php';
-echo "Session ID: " . session_id() . "<br>";
-$_SESSION['test'] = 'working';
-echo "Test value set: " . $_SESSION['test'] . "<br>";
-echo "<a href='test.php'>Check on test.php</a>";
-?>
