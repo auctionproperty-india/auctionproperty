8588ef4ed47ee6c24059c55969c5e4fc28c4a2c6
auctionproperty-india <bliveindia2018@gmail.com>
2026-08-24 12:22:10 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php
-require_once 'db.php';
-echo "Session ID: " . session_id() . "<br>";
-echo "Test value: " . ($_SESSION['test'] ?? 'NOT SET') . "<br>";
+echo "Hello! Server is working.";
+phpinfo();
 ?>
