4d2f25881774363d63b819c5a4241a4f6759e47c
auctionproperty-india <bliveindia2018@gmail.com>
2026-08-24 13:26:42 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,4 @@
 <?php
-echo "✅ PHP is working!<br>";
-require_once __DIR__ . '/db.php';
-echo "✅ Database connected!<br>";
-$stmt = $pdo->query("SELECT NOW()");
-$row = $stmt->fetch();
-echo "Server time: " . $row['now'];
+echo "Hello, server is working!";
+phpinfo();
 ?>
